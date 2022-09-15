3726678a5415fcbb6fda2681f08db64c53a42fc1
Karishmagupta05 <101394147+Karishmagupta05@users.noreply.github.com>
2022-09-15 10:53:24 +0530

    skill update

--- script.js
+++ script.js
@@ -1,4 +1,20 @@
 
+// navar
+
+const navToggler =document.querySelector(".nav-toggler");
+navToggler.addEventListener("click",()=>{
+    console.log("welcome  to my personal portfolio")
+    hideSection();
+    toggleNavbar();
+});
+function hideSection(){
+    document.querySelector("section.active").classList.toggle("fade-out");
+}
+
+function toggleNavbar(){
+    document.querySelector(".header").classList.toggle("active");
+}
+
 
 // about 
 
@@ -11,6 +27,7 @@ if(e.target.classList.contains("tab-item") && !e.target.classList.contains("acti
     tabsContainer.querySelector(".active").classList.remove("active");
     e.target.classList.add("active");
     const target=e.target.getAttribute("data-target");
+    console.log(target)
     aboutSection.querySelector(".tab-content.active").classList.remove("active");
     aboutSection.querySelector(target).classList.add("active");
 }
@@ -22,6 +39,7 @@ if(e.target.classList.contains("tab-item") && !e.target.classList.contains("acti
 document.addEventListener("click",(e)=>{
     if(e.target.classList.contains("view-project-btn")){
         toggleProjectPopup();
+        projectItemDetails(e.target.parentElement);
     }
 })
 function  toggleProjectPopup(){
@@ -31,3 +49,54 @@ function  toggleProjectPopup(){
 }
 
 document.querySelector(".pp-close").addEventListener("click",toggleProjectPopup);
+
+//click close
+
+document.addEventListener("click",(e)=>{
+    if(e.target.classList.contains("pp-inner")){
+        toggleProjectPopup();
+    }
+});
+
+
+function projectItemDetails(projectItem){
+    document.querySelector(".pp-thumbnail img").src=
+    projectItem.querySelector(".project-item-thumbnail img").src;
+
+    document.querySelector(".pp-header h3").innerHTML=
+    projectItem.querySelector(".project-item-title").innerHTML;
+
+    document.querySelector(".pp-body").innerHTML=
+    projectItem.querySelector(".project-item-details").innerHTML;
+}
+
+// const navToggler = document.querySelector(".nav-toggler");
+// navToggler.addEventListener("click", () =>{
+//     hideSection();
+// })
+// function hideSection(){
+//     document.querySelector("section.active").classList.toggle("fade-out");
+// }
+
+
+
+// active
+document.addEventListener("click" , (e)=>{
+    if(e.target.classList.contains("link-item") && e.target.hash !== ""){
+        navToggler.classList.add("hide");
+        if(e.target.classList.contains("nav-item")){
+            toggleNavbar();
+        }
+        else{
+            hideSection();
+            document.body.classList.add("hide-scrolling");
+        }
+        setTimeout(()=>{
+            document.querySelector("section.active").classList.remove("active","fade-out");
+            document.querySelector(e.target.hash).classList.add("active");
+            window.scrollTo(0,0);
+            document.body.classList.add("hide-scrolling");
+            navToggler.classList.remove("hide");
+        },500)
+    }
+})
